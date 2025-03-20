2b2a2a9a724be845b63768c3d9199ad0b3aadeaa
mathis <mathisdelbeke.2003@hotmail.com>
2025-03-20 19:22:28 +0100

    uart with interrupts and ringbuffer instead of busy waiting

--- Basics/include/uart.h
+++ Basics/include/uart.h
@@ -3,9 +3,11 @@
 
 #include <avr/io.h>
 #include <stdlib.h>
+#include <avr/interrupt.h>
 
 void uart_init();
-void uart_transmit(char c);
+void uart_transmit_busy(char c);
+void uart_transmit_interrupt(char c);
 void uart_print(const char *str);
 void uart_print_int(int32_t num);
 
