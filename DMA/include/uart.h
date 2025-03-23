6f7f4cb15c62f5e0be19161999121f21c39ee455
mathis <mathisdelbeke.2003@hotmail.com>
2025-03-23 17:35:10 +0100

    Receiving data with UART added

--- DMA/include/uart.h
+++ DMA/include/uart.h
@@ -6,9 +6,13 @@
 #include <avr/interrupt.h>
 
 void uart_init(uint32_t baud);
+
 void uart_transmit(char c);
 void uart_print_string(const char *str);
 void uart_print_signed(int32_t num);
 void uart_print_unsigned(int32_t num);
+
+char uart_read();
+uint8_t uart_available();
 
 #endif
--- DMA/src/uart.c
+++ DMA/src/uart.c
@@ -2,10 +2,15 @@
 
 #define F_CPU 16000000UL                            
 #define TX_BUFFER_SIZE 64
+#define RX_BUFFER_SIZE 64
 
 volatile char tx_buffer[TX_BUFFER_SIZE];
 volatile uint8_t tx_head = 0;
 volatile uint8_t tx_tail = 0;
+
+volatile char rx_buffer[RX_BUFFER_SIZE];
+volatile uint8_t rx_head = 0;
+volatile uint8_t rx_tail = 0;
 
 void uart_init(uint32_t baud) {
     uint16_t ubbr_val = (F_CPU / (16UL * baud)) - 1;
@@ -13,6 +18,7 @@ void uart_init(uint32_t baud) {
     UBRR0H = (ubbr_val >> 8);               // Upper 8 bits
     UBRR0L = ubbr_val;                      // Lower 8 bits
     UCSR0B = (1 << TXEN0) | (1 << UDRIE0);  // Enable TX and TX Empty Interrupt
+    UCSR0B |= (1 << RXEN0) | (1 << RXCIE0); // Enable RX and RX Complete Interrupt
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8-bit, 1 stop, no parity
 
     sei();
@@ -52,6 +58,27 @@ ISR(USART_UDRE_vect) {
         tx_tail = (tx_tail + 1) % TX_BUFFER_SIZE;
     } else {
         UCSR0B &= ~(1 << UDRIE0); 
+    }
+}
+
+char uart_read() {
+    while (rx_head == rx_tail); // Wait if buffer is empty
+
+    char c = rx_buffer[rx_tail];
+    rx_tail = (rx_tail + 1) % RX_BUFFER_SIZE;
+    return c;
+}
+
+uint8_t uart_available() {
+    return (rx_head != rx_tail);
+}
+
+ISR(USART_RX_vect) {
+    uint8_t next_head = (rx_head + 1) % RX_BUFFER_SIZE;
+    
+    if (next_head != rx_tail) {
+        rx_buffer[rx_head] = UDR0;
+        rx_head = next_head;
     }
 }
 
