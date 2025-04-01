07b6a85f4d2aaf8a79903efb7a27111798fc3747
mathis <mathisdelbeke.2003@hotmail.com>
2025-04-01 18:13:56 +0200

    Write and Read to/from slave

--- I2Cmaster/src/main.c
+++ I2Cmaster/src/main.c
@@ -2,29 +2,36 @@
 #include "I2C_master.h"
 #include <util/delay.h>
 
+#define WRITE_ADDRESS ((0x18 << 1) | 0)
+#define READ_ADDRESS ((0x18 << 1) | 1)
+
 int main() {
 	uart_init(9600);
 	i2c_init();
 
 	char message[] = "Hello Slave\r\n";
-	uint8_t data = 0;
+	uint8_t character = 1;
 
 	while(1) {
 		i2c_start();
-		i2c_write_address(0x30);
+		i2c_write_address(WRITE_ADDRESS);
 		for (uint8_t i = 0; message[i] != '\0'; i++) {
 			i2c_write_data(message[i]);
 		}
+		i2c_stop();
+
+		_delay_ms(2000);	
+		
+		i2c_start();
+		i2c_write_address(READ_ADDRESS);
+		do {
+			character = i2c_read_data(1);
+			uart_transmit(character);
+		} while (character != '\0');
+		i2c_read_data(0);
 		i2c_stop();
 
 		_delay_ms(2000);
 	}
 	return 0;
 }
-	/*
-		i2c_start();
-		i2c_write_address(0x11);
-		data = i2c_read_data(0);
-		uart_print_unsigned(data);
-		i2c_stop();
-		*/
--- I2Cslave/src/I2C_slave.c
+++ I2Cslave/src/I2C_slave.c
@@ -1,47 +1,52 @@
 #include "I2C_slave.h"
 #include "uart.h"
 
-#include <util/delay.h>
+#define SLAVE_ADDRESS 0x18
 
-#define SLAVE_ADDRESS (0x30 >> 1)
-
+volatile uint8_t send_index = 0;
+volatile char message[] = "Hello Master\r\n";
 static char received_data = 0;
 
 void i2c_slave_init(uint8_t address) {
     TWAR = (address << 1);      // Load slave address (shifted for 7-bit addressing)
     TWCR = (1 << TWEA) |        // Enable ACK
            (1 << TWEN) |        // Enable TWI
-           (1 << TWIE);         // Enable TWI interrupt
+           (1 << TWIE) |         // Enable TWI interrupt
+           (1 << TWINT);
     sei();                      // Enable global interrupts
 }
 
 ISR(TWI_vect) {
-    switch (TWSR & 0xF8) {  // Mask out prescaler bits and check status
-        case 0x60:  // SLA+W received (master wants to write)
-        case 0x68:  // Arbitration lost, SLA+W received
-            TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT) | (1 << TWIE); // Acknowledge and wait for data
+    switch (TWSR & 0xF8) {
+        case 0xA8:  // Own SLA+R has been received, ACK sent
+        case 0xB8:  // Data byte has been transmitted, ACK received
+            TWDR = message[send_index++];  // Load next byte
+            TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT) | (1 << TWIE);  // Enable ACK for next byte
             break;
 
-        case 0x80:  // Data received, ACK returned
-        case 0x88:  // Data received, NACK returned
+        case 0xC8:  // Data byte has been transmitted, NACK received (last byte sent)
+            send_index = 0;  // Reset index for next transaction
+            TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT) | (1 << TWIE);  // Enable ACK for next request
+            break;
+
+        case 0x60:  // Own SLA+W has been received, ACK sent
+            TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT) | (1 << TWIE);
+            break;
+
+        case 0x80:  // Data byte received, ACK sent
             received_data = TWDR;  // Read data
             uart_transmit(received_data);
-            TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT) | (1 << TWIE); // ACK next byte
+            TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT) | (1 << TWIE);
             break;
 
-        case 0xA8:  // SLA+R received, master wants data
-        case 0xB0:  // Arbitration lost, SLA+R received
-            TWDR = 0x55;  // Example data to send
-            TWCR = (1 << TWEN) | (1 << TWINT) | (1 << TWIE); // Clear flag to send data
+        case 0x88:  // Data byte received, NACK sent (end of reception)
+        case 0xA0:  // Stop or repeated start received
+            send_index = 0;  // Reset index in case of repeated communication
+            TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT) | (1 << TWIE);
             break;
 
-        case 0xC0:  // Data transmitted, NACK received (end of transmission)
-        case 0xC8:  // Data transmitted, ACK received (continue sending)
-            TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT) | (1 << TWIE); // Ready for next transmission
-            break;
-
-        case 0xA0:  // STOP or repeated START received
-            TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT) | (1 << TWIE); // Reset TWI logic
+        default:
+            TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT) | (1 << TWIE);  // Default case to keep I2C working
             break;
     }
 }
