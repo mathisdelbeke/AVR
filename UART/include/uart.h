b6f057d4a19ee4c52e30659f0cc45111e00b36c1
mathis <mathisdelbeke.2003@hotmail.com>
2025-04-14 17:35:20 +0200

    Fix parameter of printing unsigned

--- UART/include/uart.h
+++ UART/include/uart.h
@@ -10,7 +10,7 @@ void uart_init(uint32_t baud);
 void uart_transmit(char c);
 void uart_print_string(const char *str);
 void uart_print_signed(int32_t num);
-void uart_print_unsigned(int32_t num);
+void uart_print_unsigned(uint32_t num);
 
 char uart_read();
 uint8_t uart_available();
